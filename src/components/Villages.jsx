d334a6b6c7d6bf99aeb1e6f6a42c261df103f385
Anastasia Varoucha <cs04288@uoi.gr>
2024-07-11 16:49:02 +0300

    Add village cards and assets

--- src/components/Villages.jsx
+++ src/components/Villages.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import "../styles/Villages.css";
+import CreateMapCard from './CreateMapCard.jsx';
+import { useEffect, useState } from 'react';
+import {getQuery} from './api.jsx';
 
 function Villages() {
+  const [data, setData] = useState([]);  
+    const [isLoading, setIsLoading] = useState(true);
+
+    console.log(data);
+
+    useEffect(() => {
+        const runQuery = async () => {
+        setData(await getQuery());
+        };
+
+        runQuery()
+        .then(() => {
+            setTimeout(() => setIsLoading(false), 4500);
+        })
+        .catch((error) => console.log(error));
+    }, []);
   return (
-    <div className="villages-container">
-      <h1>Τα Χωριά</h1>
-    </div>
+    <div className='panigiria-container'>
+            <h1 className='panigiria-heading'>Τα Χωριά</h1>
+            <dl className='panigiria-cards'>
+                {data && data.filter(item => item.id >= 16 && item.id <= 18)
+            .map(item => <CreateMapCard key={item.id} data={item}/>)}</dl>
+        </div>
   );
 }
 
